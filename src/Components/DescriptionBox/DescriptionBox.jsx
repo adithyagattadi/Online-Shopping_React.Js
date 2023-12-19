a3dc5e0c10223605a84dd9143579072205a9347e
Adithya Gattadi <adithyagattadi@Adithyas-MacBook-Air.local>
2023-12-19 21:45:46 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Explore an enticing online shopping experience with our website, where a vast array of products awaits your discovery. From the latest fashion trends and cutting-edge electronics to home essentials and beyond, our platform offers a diverse selection to cater to your every need. Enjoy the convenience of seamless navigation, secure transactions, and swift deliveries. Immerse yourself in a user-friendly environment designed to elevate your online shopping journey.</p>
+        <p>Elevate your shopping experience with us - where quality, variety, and convenience converge.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
